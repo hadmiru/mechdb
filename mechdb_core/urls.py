b52c0f56fbde13c20c6976066a6e36a57ed19486
hadmiru <roman.ostanin@gmail.com>
2018-06-03 00:30:14 +0300

    Добавлено меню. Небольшие изменения в дизайне шаблонов форм

--- mechdb_core/urls.py
+++ mechdb_core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index_page, name='index_page'),
     # === контейнеры ===
-    url('map/', views.containers_map, name='containes_map'),
+    url(r'^map/', views.containers_map, name='containers_map'),
     url(r'^place/(?P<pk>\d+)/$', views.container_detail, name='container_detail'),
     url(r'^place/new/$', views.container_new, name='container_new'),
     url(r'^place/(?P<pk>\d+)/edit/$', views.container_edit, name='container_edit'),
